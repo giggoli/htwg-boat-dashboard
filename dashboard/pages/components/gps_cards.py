6709aac0c5a35a00245823c55103b9ae83da8172
giggoli <65236454+giggoli@users.noreply.github.com>
2023-03-11 13:18:59 +0100

    added gps cards, add page

---
+++ dashboard/pages/components/gps_cards.py
@@ -0,0 +1,103 @@
+from dash import dcc, html
+import dash_bootstrap_components as dbc
+
+
+
+gps_HEAD = dbc.Card(
+    [
+        dbc.CardBody(
+            [
+                html.H4(children="Heading"),
+                html.Div(id="gps_HEAD", children="no Value received")
+            ]
+        ),
+    ],
+)
+
+gps_VALID = dbc.Card(
+    [
+        dbc.CardBody(
+            [
+                html.H4(children="Validity"),
+                html.Div(id="gps_VAILD", children="no Value received")
+            ]
+        ),
+    ],
+)
+
+gps_LAT = dbc.Card(
+    [
+        dbc.CardBody(
+            [
+                html.H4(children="Latitude"),
+                html.Div(id="gps_LAT", children="no Value received")
+            ]
+        ),
+    ],
+)
+
+gps_NS = dbc.Card(
+    [
+        dbc.CardBody(
+            [
+                html.H4(children="North/South"),
+                html.Div(id="gps_NS", children="no Value received")
+            ]
+        ),
+    ],
+)
+
+gps_LONG = dbc.Card(
+    [
+        dbc.CardBody(
+            [
+                html.H4(children="Longitude"),
+                html.Div(id="gps_LONG", children="no Value received")
+            ]
+        ),
+    ],
+)
+
+gps_EW = dbc.Card(
+    [
+        dbc.CardBody(
+            [
+                html.H4(children="Eath/West"),
+                html.Div(id="gps_EW", children="no Value received")
+            ]
+        ),
+    ],
+)
+
+gps_SPEED = dbc.Card(
+    [
+        dbc.CardBody(
+            [
+                html.H4(children="Speed over ground"),
+                html.Div(id="gps_SPEED", children="no Value received")
+            ]
+        ),
+    ],
+)
+
+gps_VARIAT = dbc.Card(
+    [
+        dbc.CardBody(
+            [
+                html.H4(children="Variation"),
+                html.Div(id="gps_VARIAT", children="no Value received")
+            ]
+        ),
+    ],
+)
+
+gps_VARIAT_EW = dbc.Card(
+    [
+        dbc.CardBody(
+            [
+                html.H4(children="Variation East/West"),
+                html.Div(id="gps_VARIAT_EW", children="no Value received")
+            ]
+        ),
+    ],
+)
---
+++ dashboard/pages/gps.py
@@ -0,0 +1,40 @@
+import dash
+from dash import dcc, html
+import dash_bootstrap_components as dbc
+
+import pages.components.gps_cards as gps_cards
+
+dash.register_page(__name__, name='GPS',order=3)
+
+layout = html.Div(
+    children=[
+        dbc.Row(
+            [
+                dbc.Col([
+                    html.H3(children="GPS")
+                ],xs=12, sm=12, md=12, lg=12, xl=12, xxl=12)
+            ], className="row-title"
+        ),
+        dbc.Row(
+            [
+                dbc.Col([gps_cards.gps_HEAD],xs=4, sm=4, md=4, lg=4, xl=4, xxl=4),
+                dbc.Col([gps_cards.gps_VALID],xs=4, sm=4, md=4, lg=4, xl=4, xxl=4),
+                dbc.Col([gps_cards.gps_SPEED],xs=4, sm=4, md=4, lg=4, xl=4, xxl=4),
+            ], className="row-buffer"
+        ),
+        dbc.Row(
+            [
+                dbc.Col([gps_cards.gps_LAT],xs=4, sm=4, md=4, lg=4, xl=4, xxl=4),
+                dbc.Col([gps_cards.gps_NS],xs=4, sm=4, md=4, lg=4, xl=4, xxl=4),
+                dbc.Col([gps_cards.gps_VARIAT],xs=4, sm=4, md=4, lg=4, xl=4, xxl=4),
+            ], className="row-buffer"
+        ),
+        dbc.Row(
+            [
+                dbc.Col([gps_cards.gps_LONG],xs=4, sm=4, md=4, lg=4, xl=4, xxl=4),
+                dbc.Col([gps_cards.gps_EW],xs=4, sm=4, md=4, lg=4, xl=4, xxl=4),
+                dbc.Col([gps_cards.gps_VARIAT_EW],xs=4, sm=4, md=4, lg=4, xl=4, xxl=4),
+            ], className="row-buffer"
+        ),
+    ],
+)
