9ca4790d102f3eda68c572d72fef9b5bd8fcc909
giggoli <65236454+giggoli@users.noreply.github.com>
2023-04-12 12:08:43 +0200

    load ws paths from config

--- ws_server/websocket_server.py
+++ ws_server/websocket_server.py
@@ -1,35 +1,28 @@
 import asyncio
 import websockets
-import json
+import sys, os
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+import configloader.config as cnf
 
 
-port = 8123
 connected_paths = dict()
-
-paths = ["/", "/test", "/battery"]
-
+paths = list()
 
 def setup_paths():
     for path in paths:
         connected_paths[path] = set()
-
+    
+def convert_topic_to_ws_path(topics):
+    for topic in topics:
+        paths.append(f"/{topic}")
 
 async def send_to_connections(connections, websocket, message):
-    # This is just an example
-    a = {
-         "eins": 22,
-         "zwei": "value"
-    }
-    a = json.dumps(a)
     # Send a response to all connected clients except sender
     for conn in connections:
            if conn != websocket:
-               await conn.send(a)
-
-
-async def handler(message, websocket, connections):
-    await send_to_connections(connections, websocket, message)
-    return "parsed msg"
+               await conn.send(message)
 
 
 async def message_handler(websocket, path):
@@ -37,8 +30,7 @@ async def message_handler(websocket, path):
     connections.add(websocket)
     try:
         async for msg in websocket:
-            parsed_msg = await handler(msg, websocket, connections)
-            print("Received message from client: " + parsed_msg +" path: "+ path)
+            await send_to_connections(connections, websocket, msg)
 
     except websockets.exceptions.ConnectionClosed as e:
         pass
@@ -48,17 +40,19 @@ async def message_handler(websocket, path):
 
 
 async def ws_handler(websocket, path):
-    match path:
-        case "/battery":
-            await message_handler(websocket, path)
-        case _:
-            print("This path is not defined")
+    if path in paths:
+        await message_handler(websocket, path)
+    else:
+        print(f"The path: {path} is not defined")
+        
 
 
-async def main():
+async def main(port):
     async with websockets.serve(ws_handler, "localhost", port):
         await asyncio.Future()
 
 if __name__ == "__main__":
+    port, topics = cnf.getWsConfig()
+    convert_topic_to_ws_path(topics)
     setup_paths()
-    asyncio.run(main())
+    asyncio.run(main(port))
